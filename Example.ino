180dd5236db011efcae342d6cd73f31cb773b443
Kostas <37267037+ItsAntiUp@users.noreply.github.com>
2023-11-14 16:40:27 +0200

    Update Example.ino

--- Example.ino
+++ Example.ino
@@ -4,23 +4,40 @@ const int MATRIX_DIN_PIN = 13;
 const int MATRIX_CLK_PIN = 12;
 const int MATRIX_CS_PIN = 11;
 
-LEDMatrix8x8 matrix(MATRIX_DIN_PIN, MATRIX_CLK_PIN, MATRIX_CS_PIN);
+LEDMatrix8x8::Brightness BRIGHTNESS = LEDMatrix8x8::B_MINIMAL;
+
+LEDMatrix8x8 matrix(MATRIX_DIN_PIN, MATRIX_CLK_PIN, MATRIX_CS_PIN, BRIGHTNESS);
 
 void setup() {
     matrix.begin();
 }
 
 void loop() {
-    // matrix.displayCharacter('A', 2000);
-    // matrix.displayCharacter('B', 2000);
-    // matrix.displayCharacter('C', 2000);
-    // matrix.displayCharacter('9', 2000);
+    // Displaying individual characters, horizontally.
+    matrix.displayCharacter('A', 2000, matrix.HORIZONTAL);
+    matrix.displayCharacter('B', 2000, matrix.HORIZONTAL);
+    matrix.displayCharacter('C', 2000, matrix.HORIZONTAL);
 
-    // matrix.displaySentence("bb11256 45", 1000);
 
-    matrix.displayAnimation(matrix.TYPE_ONE, matrix.RIGHT, 5000);
+    // Displaying animation (RHOMBUS, horizontal orientation)
+    matrix.displayAnimation(3000, matrix.RHOMBUS, matrix.HORIZONTAL);
 
-    matrix.displaySentence("HELLO WORLD", 500); // Display "HELLO WORLD" with a delay of 500ms between characters
+    // Displaying animation (LOADING_BAR, vertical orientation)
+    matrix.displayAnimation(3000, matrix.LOADING_BAR, matrix.VERTICAL);
 
-    matrix.ScrollText("HELLO WORLD", 500);
+    // Displaying animation (UP_ARROW, vertical orientation)
+    matrix.displayAnimation(3000, matrix.UP_ARROW, matrix.VERTICAL);
+
+    // Displaying animation (LEFT_ARROW, horizontal orientation)
+    matrix.displayAnimation(3000, matrix.LEFT_ARROW, matrix.HORIZONTAL);
+
+
+    // Displaying text for some amount of time, horizontally.
+    matrix.displayText("HELLO ", 500, matrix.HORIZONTAL);
+
+    // Scrolling text with 100ms interval between frames, vertically.
+    matrix.scrollText("WORLD ", 100, matrix.VERTICAL);
+
+    // Scrolling text with 100ms interval between frames, horizontally.
+    matrix.scrollText("ARDUINO ", 100, matrix.HORIZONTAL);
 }
